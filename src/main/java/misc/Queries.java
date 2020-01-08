b96d5177a96c9eec795869b27b52dadfc52557ff
Tchiring Pemba Lama <tchiring.lama@ekbana.info>
2020-01-08 16:11:05 +0545

    Test Fetch_sale api

--- src/main/java/misc/Queries.java
+++ src/main/java/misc/Queries.java
@@ -17,7 +17,7 @@ public class Queries {
     public String fetchSale (String start_date, String end_date, ArrayList<String> site_code){
         String temp = StringUtils.join(site_code, ',');
         System.out.println(temp);
-        return "select * from mmpl.V_EKB_CUST_SALE where billdate >= "+ start_date +" and billdate < "+ end_date + " and admsite_code in (" + StringUtils.join(site_code, ',') + ")";
+        return "select * from mmpl.V_EKB_CUST_SALE where billdate >= '"+ start_date +"' and billdate < '"+ end_date + "' and admsite_code in (" + StringUtils.join(site_code, ',') + ")";
     }
 
     public String fetchTransactionRecord (String table_name,int offset_value,int range_count){
