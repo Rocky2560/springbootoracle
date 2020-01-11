c9413f5bdf60085c500b5fff72f920947ada87e3
Tchiring Pemba Lama <tchiring.lama@ekbana.info>
2020-01-11 11:35:19 +0545

    Change sales return type format

--- src/main/java/misc/Queries.java
+++ src/main/java/misc/Queries.java
@@ -15,8 +15,8 @@ public class Queries {
 
     public String fetchBillInfo(String start_date, String end_date){
 //        return "select BILLNO, BILLDATE, ADMSITE_CODE, TOATALDISCOUNTAMT, LPCARDNO, CAT1, ICODE, SALEQTY from mmpl.V_EKB_CUST_SALE where billdate >='"+start_date+"' and billdate < '"+end_date+"'" ;
-//        return "select BILLNO, BILLDATE, SITE_CODE, NETSALES, LPCARDNO, NETSALES from mmpl.V_EKB_POSBILL where billdate >='"+start_date+"' and billdate < '"+end_date+"'" ;
-        return "select BILLNO, BILLDATE, SITE_CODE, NETSALES, LPCARDNO, NETSALES from mmpl.V_EKB_POSBILL as bill FULL OUTER JOIN mmpl.V_EKB_CUST_SALE as sale ON bill.BILLNO=sale.BILLNO where billdate >='"+start_date+"' and billdate < '"+end_date+"'" ;
+        return "select BILLNO, BILLDATE, SITE_CODE, NETSALES, LPCARDNO, NETSALES from mmpl.V_EKB_POSBILL where billdate >='"+start_date+"' and billdate < '"+end_date+"'" ;
+//        return "select BILLNO, BILLDATE, SITE_CODE, NETSALES, LPCARDNO, NETSALES from mmpl.V_EKB_POSBILL as bill FULL OUTER JOIN mmpl.V_EKB_CUST_SALE as sale ON bill.BILLNO=sale.BILLNO where billdate >='"+start_date+"' and billdate < '"+end_date+"'" ;
     }
     public String fetchProductInfo(String bill_no){
 //        return "select BILLNO, BILLDATE, ADMSITE_CODE, TOATALDISCOUNTAMT, LPCARDNO, CAT1, ICODE, SALEQTY from mmpl.V_EKB_CUST_SALE where billdate >='"+start_date+"' and billdate < '"+end_date+"'" ;
